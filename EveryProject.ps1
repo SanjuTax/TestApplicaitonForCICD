3eab4c02722df2ba721449b7c009cb0d488478d4
SanjuTax <sanjith.chidhirala@tax.virginia.gov>
2024-06-21 12:04:12 -0400

    Update EveryProject.ps1

--- EveryProject.ps1
+++ EveryProject.ps1
@@ -3,7 +3,9 @@ Write-Output "Every Project power shell script triggered"
 # defining the paths to each folder
 $ReleaseFolder = 'C:\Jenkins\workspace\Sanjith_test\Release'
 $ConfiguredFolder = 'C:\Jenkins\workspace\Sanjith_test\Configured'
-$BackupFolder = 'C:\Jenkins\workspace\Sanjith_test\Backup'
+$BackupRoot = 'C:\Jenkins\workspace\Sanjith_test\Backup'
+$currentDate = Get-Date -Format "MMddyyyy"
+$BackupFolder = $BackupRoot+$currentDate
 
 #check if configured folder is present
 if (-not (Test-Path -Path $ConfiguredFolder)) {
@@ -12,11 +14,28 @@ if (-not (Test-Path -Path $ConfiguredFolder)) {
 }
 #Write-Output "Modifying Configured Folder at $ConfiguredFolder"
 
-#Creating a backup
+#Creating a backupFolder
 if (-not (Test-Path -Path $BackupFolder)) {
      Write-Output "Creating BackupFolder at $BackupFolder"
      New-Item -ItemType Directory -Path $BackupFolder
 }
 
-Write-Output "Created"
+# Create a temporary folder for zipping
+Write-Output "Creating tempFolder at $tempFolder"
+$tempFolder = New-TemporaryFile
+Remove-Item -Path $tempFolder
+New-Item -ItemType Directory -Path $tempFolder
+
+# Move the source folder to the temporary folder (cut operation)
+Move-Item -Path $ReleaseFolder -Destination $tempFolder
+
+# Create a zip file from the moved folder
+Write-Output "Creating Zip at $BackupFolder"
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::CreateFromDirectory($tempFolder, $BackupFolder)
+
+# Clean up the temporary folder
+Remove-Item -Path $tempFolder -Recurse
+
+Write-Output "Folder has been zipped and moved to $zipFilePath"
 
