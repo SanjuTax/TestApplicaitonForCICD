c4721d20930884f4260a23b064f27c464062e9d0
SanjuTax <sanjith.chidhirala@tax.virginia.gov>
2024-06-21 14:57:23 -0400

    Update EveryProject.ps1

--- EveryProject.ps1
+++ EveryProject.ps1
@@ -40,5 +40,5 @@ Remove-Item -Path $tempFolder -Recurse
 Write-Output "Folder has been zipped and moved to $zipFilePath"
 
 # Update to the latest version
+Write-Output "Getting the latest version from Release"
 Copy-Item -Path $ReleaseFolder -Destination $ConfiguredFolder
-
